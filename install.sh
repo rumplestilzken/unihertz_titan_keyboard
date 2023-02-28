c3315a195353f2a0b664c5ebe4c65504ff6722b8
rumplestilzken <rumplestilzken@gmail.com>
2023-02-28 16:43:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ adb shell mount -o remount,rw /
 adb push resources/keyboard/Android10_function/system_usr_idc/* /system/usr/idc/
 adb push resources/keyboard/Android10_function/system_usr_keychars/* /system/usr/keychars/
 adb push resources/keyboard/Android10_function/system_usr_keylayout/* /system/usr/keylayout/
-adb push resources/uinput-titan/uinput-titan /system/bin/uinput-titan
-adb push resources/uinput-titan/titan.rc /system/etc/init/
-adb push resources/uinput-titan/titan-uinput.idc /system/usr/idc/
+# adb push resources/uinput-titan/uinput-titan /system/bin/uinput-titan
+# adb push resources/uinput-titan/titan.rc /system/etc/init/
+# adb push resources/uinput-titan/titan-uinput.idc /system/usr/idc/
 # adb reboot
