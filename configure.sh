be3c95fcf1689899de01a7973d1c321d44327d44
Nick McSherry <nick.mcsherry@gmail.com>
2023-03-09 09:20:45 -0500

    Updated configure.sh with hardware/software keyboard setting

--- configure.sh
+++ configure.sh
@@ -3,3 +3,5 @@ adb shell settings put system screen_off_timeout 600000
 adb shell settings put system display_density_forced 320
 adb shell settings put system system navigation_mode 2
 adb shell settings put system --lineage navigation_bar_hint 0
+adb shell settings put secure show_ime_with_hard_keyboard 1
+echo Configure Complete.
